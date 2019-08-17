d73e0bf5bdb3e2423ef9b3cf031d83743030d2c5
Atlas <AtlasJan@gmx.com>
2019-08-18 00:47:46 +0100

    work on mocha file

--- test/index.js
+++ test/index.js
@@ -2,3 +2,21 @@ const galex = require('..');
 var metalsmith, assert;
 metalsmith = require('metalsmith');
 assert = require('assert');
+writemetadata = require('writemetadata');
+
+function er(e) { if(e) return e; }
+
+function t1() {
+  metalsmith(__dirname)
+   .source('source')
+   .destination('out')
+   .use(galex())
+   .use(write-metadata({
+     pattern: ['**/*'],
+     bufferencoding: 'utf-8'
+   }))
+   .build(er);
+}
+
+describe('metalsmith-galex', => {
+  it('should output a galex object', t1);
